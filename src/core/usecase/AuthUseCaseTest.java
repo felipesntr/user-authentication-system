f67304feb02c0046d4bb5eff857da52e9fc72449
Felipe S <rfelipe273@gmail.com>
2021-12-05 06:58:23 -0300

    ensure AuthUseCase throws if an empty password is provided.

--- src/core/usecase/AuthUseCaseTest.java
+++ src/core/usecase/AuthUseCaseTest.java
@@ -12,6 +12,8 @@ public class AuthUseCaseTest {
         public String auth(String email, String password) throws MissingParamError {
             if (email == "")
                 throw new MissingParamError("email");
+            if (password == "")
+                throw new MissingParamError("password");
             return "";
         }
     }
@@ -20,5 +22,11 @@ public class AuthUseCaseTest {
     public void should_throw_if_an_empty_email_is_provided() throws MissingParamError {
         AuthUseCase authUseCase = new AuthUseCase();
         authUseCase.auth("", "valid_password");
+    }
+
+    @Test(expected = MissingParamError.class)
+    public void should_throw_if_an_empty_password_is_provided() throws MissingParamError {
+        AuthUseCase authUseCase = new AuthUseCase();
+        authUseCase.auth("valid_email@email.com", "");
     }
 }
