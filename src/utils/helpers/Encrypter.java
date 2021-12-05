045eb9699b3697facadce31387311d23923808b6
Felipe S <rfelipe273@gmail.com>
2021-12-05 08:29:17 -0300

    added bcrypt lib.

--- src/utils/helpers/Encrypter.java
+++ src/utils/helpers/Encrypter.java
@@ -1,9 +1,17 @@
 package utils.helpers;
 
+import org.springframework.security.crypto.bcrypt.BCrypt;
+
 public class Encrypter {
     public boolean is_valid_password = false;
+    public String value;
+    public String hash;
 
     public boolean compare(String value, String hash) {
-        return true;
+        boolean result = BCrypt.checkpw(value, hash);
+        if (result) {
+            this.is_valid_password = true;
+        }
+        return result;
     }
 }
